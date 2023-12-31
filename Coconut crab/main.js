cbea996955e5300e44b4253319d382e801d4fb92
Soooham <sohamvam@gmail.com>
2023-12-30 18:31:19 -0800

    testing fetching

--- Coconut crab/main.js
+++ Coconut crab/main.js
@@ -1,8 +1,11 @@
 "use strict";
 import add from "./util.mjs";
 
-(() => {
+(async () => {
 
     console.log(add(2, 5));
+    await fetch("./test.json").then((res) => {
+        console.log(res.json());
+    })
 
 })();
